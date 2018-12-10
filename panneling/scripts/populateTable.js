ec5180b4d7fcff8b49a8b2958b886de93d938b86
kingkashyap <kingkashyap39@gmail.com>
2018-12-10 18:45:42 +0530

    newly added

--- panneling/scripts/populateTable.js
+++ panneling/scripts/populateTable.js
@@ -1,7 +1,7 @@
 var addressDetails = function () {
 
     var httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", "assets/address.json", true);
+    httpRequest.open('GET', 'assets/address.json', true);
     httpRequest.send();
     httpRequest.onreadystatechange = function () {
 
@@ -18,7 +18,7 @@ var addressDetails = function () {
 var personDetails = function () {
 
     var httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", "assets/person.json", true);
+    httpRequest.open('GET', 'assets/person.json', true);
     httpRequest.send();
     httpRequest.onreadystatechange = function () {
 
@@ -62,7 +62,7 @@ var loadPerson = function() {
 }
 
 var constructAddress = function(addressJson) {
-    
+
     var constructTable = '<tr><th>Street</th><th>City</th><th>Postal Code</th></tr>';
 
     for(var i in addressJson) {
@@ -87,7 +87,7 @@ var constructAddress = function(addressJson) {
  }
 
 var constructPerson = function(personJson) {
-    
+
     var constructTable = '<tr><th>First Name</th><th>Last Name </th><th>email</th><th>Birth Date</th></tr>'
                          
     for (var i in personJson) {
@@ -102,4 +102,57 @@ var constructPerson = function(personJson) {
     loadPerson();
 }
 
+var addressSubmit = function() {
+
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'assets/address.json', true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+
+        if(httpRequest.readyState == 4) {
+
+            var addressJson = JSON.parse(httpRequest.responseText);
+            console.log(addressJson);
+            var street = document.getElementById('street').value;
+            var city = document.getElementById('city').value;
+            var code = document.getElementById('code').value;
+            var address = {
+                           'street' : street,
+                           'city' : city,
+                           'postal_code' : code,
+                          }
+            console.log(address);
+            addressJson.push(address);
+            constructAddress(addressJson);
+        }
+    }
+}
+
+var personSubmit = function() {
+
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'assets/person.json', true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+
+        if(httpRequest.readyState == 4) {
+
+            var personJson = JSON.parse(httpRequest.responseText);
+            console.log(personJson);
+            var firstName = document.getElementById('firstname').value;
+            var lastName = document.getElementById('lastname').value;
+            var email = document.getElementById('email').value;
+            var dob = document.getElementById('dob').value;
+            var person = {
+                          'firstName' : firstName,
+                          'lastName' : lastName,
+                          'email' : email,
+                          'birth_date' : dob,
+                          }
+            console.log(person);
+            personJson.push(person);
+            constructPerson(personJson);
+        }
+    }
+}
 
